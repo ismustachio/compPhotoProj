2750d01032b6963bce4c7de5090c8b7e617a2c1e
Armando Lajara <lajara52@gmail.com>
2020-06-04 09:14:18 -0700

    typos

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# compPhotoProj
+Computational Photography Project
+This project attempts to act as a command-line utility, image filter,
+application. The program has the following avaliable image filter:
+	LeftSobel
+	RightSobel
+	TopSobel
+	BottomSobel
+	Gaussian
+	Emboss
+	Identity
+	Outline
+	Sharp
+	Blur
+	Custom (3x3)
+To maxize this utility, it is best to run against
+large quantaties of images. This utility will spawned a thread
+(goroutine) per each image using all available system resources.
+
+
+Example Usage:
+  ./compPhoto -p image.png 	-f Emboss -r 3
+  ./compPhoto -p ./testImages/ -f Identity
+  ./compPhoto -p image1.png -c '.3 .2 -1 0 1 1 .8 .2 .2' -r 10
+
+
--- compPhoto.go
+++ compPhoto.go
@@ -103,8 +103,8 @@ func Gray(img image.Image, f string, format string) int {
 
 var sema = make(chan struct{}, 20)
 var fpath = flag.String("p", ".", "path to single image or directory with images")
-var operation = flag.String("f", "", "Avaliable kernel's: \n[LRTB]Sobel\nBlur\nSharp\nOutline\nIdentity\nEmboss\nAvaliable Image Transformation:\nGrayScale")
-var runTime = flag.Int("r", 1, "times to run the given convulotion kernel")
+var operation = flag.String("f", "", "Available kernel's: \n[LRTB]Sobel\nBlur\nSharp\nOutline\nIdentity\nEmboss\nAvailable Image Transformation:\nGrayScale")
+var runTime = flag.Int("r", 1, "times to run the given convolution kernel")
 var custom = flag.String("c", "", "Use custom 3x3 matrix. Ex: -c '3 2 -1 8 9 2 1 -9 1' ")
 
 func main() {
